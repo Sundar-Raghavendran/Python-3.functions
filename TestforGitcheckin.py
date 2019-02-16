7b28441a317fb5fb2cc1423ea3c4d216d2d11507
Sundar Raghavendran <sundar-r@Sundars-MacBook-Air.local>
2019-02-16 11:14:36 -0600

    get input from user and find the area

--- TestforGitcheckin.py
+++ TestforGitcheckin.py
@@ -1,4 +1,5 @@
 import math
 
-area=float(math.pi*math.pow(5,2))
+radius = float(input("what is the radius of the circle?: "))
+area=float(math.pi*math.pow(radius,2))
 print("area of a circle with radius 5 is:",round(area,3))
