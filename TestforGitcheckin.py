131a9520dd071788be8a66f7e883909728ca2e27
Sundar Raghavendran <sundar-r@Sundars-MacBook-Air.local>
2019-02-16 11:16:52 -0600

    print user inout and the area

--- TestforGitcheckin.py
+++ TestforGitcheckin.py
@@ -2,4 +2,4 @@ import math
 
 radius = float(input("what is the radius of the circle?: "))
 area=float(math.pi*math.pow(radius,2))
-print("area of a circle with radius 5 is:",round(area,3))
+print("area of a circle with radius ", radius, " is:",round(area,3))
